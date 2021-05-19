3e794da067240214bb5821d1769b0f2944f51dec
root <root@ip-172-31-26-241.us-east-2.compute.internal>
2021-05-19 09:49:52 +0000

    Some confusion

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 print("This is master branch")
+<<<<<<< Updated upstream
 print("This was intended for master branch")
+=======
+print("This is another change for add branch")
+>>>>>>> Stashed changes
